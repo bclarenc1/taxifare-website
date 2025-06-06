baeef3bd052d930f4f9805233c7b207115f8fcb6
benjamin clarenc <benjamin.clarenc@gmail.com>
2025-06-06 15:22:22 +0200

    Feat: added map; reworked layout)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 import requests
-import datetime
+import pandas as pd
 from math import sin, cos, sqrt, atan2, radians
 
 KM_TO_MILES = 0.621371
@@ -19,9 +19,8 @@ def compute_dist(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
     return R * c
 
 # @TODO
-# add map
 # cache stuff
-
+st.title("(Ill-trained) ride fare estimator")
 st.markdown("## Please fill in with your needs:")
 
 st.markdown("### Time")
@@ -33,10 +32,12 @@ pickup_longitude  = st.number_input("Pick-up longitude:",  value=-73.950, step=0
 pickup_latitude   = st.number_input("Pick-up latitude:",   value=+40.750, step=0.001, format="%.3f", min_value=-90.,  max_value=90.)
 dropoff_longitude = st.number_input("Drop-off longitude:", value=-73.990, step=0.001, format="%.3f", min_value=-180., max_value=180.)
 dropoff_latitude  = st.number_input("Drop-off latitude:",  value=+40.950, step=0.001, format="%.3f", min_value=-90.,  max_value=90.)
-st.markdown("### For whom")
-passenger_count   = st.number_input("Passenger count:",    value=1, min_value=0, max_value=69)
 
-st.markdown("### >> Results")
+route_df = pd.DataFrame({"lon":[pickup_longitude, dropoff_longitude],
+                         "lat":[pickup_latitude, dropoff_latitude],
+                         "color":[(255,0,0), (0,255,0)]})
+st.map(route_df, latitude="lat", longitude="lon", size=20, color="color")
+st.markdown(":red[start point] -> :green[end point]")
 distance = compute_dist(pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude)
 unit = "km"
 if st.checkbox("Use miles", key="chbox_use_miles"):
@@ -44,6 +45,10 @@ if st.checkbox("Use miles", key="chbox_use_miles"):
     unit = "mile" if distance == 1. else "miles"
 st.write(f"Ride distance: {distance:.2f} {unit}")
 
+st.markdown("### For whom")
+passenger_count   = st.number_input("Passenger count:",    value=1, min_value=0, max_value=69)
+
+st.markdown("### >> Results")
 URL = "https://taxifare-743320960469.europe-west1.run.app/predict"
 # LEWAGON_URL = "https://taxifare.lewagon.ai/predict"
 
